3dd4e739219d0137d9c7c3f6424015bc06822c5a
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2018-05-24 18:12:51 +0200

    Moke JMH benchmark infrastructure

--- src/main/scala/communitybench/Benchmark.scala
+++ src/main/scala/communitybench/Benchmark.scala
@@ -4,20 +4,43 @@ import scala.{Any, Array, Unit, Int, Long}
 import java.lang.{String, System}
 import scala.Predef.assert
 import scala.Predef.augmentString
+import org.openjdk.jmh.annotations._
 
 abstract class Benchmark {
-  def run(input: String): Any
+  var input: String = _
 
-  final def main(args: Array[String]): Unit = {
+  /** One iteration of this benchmark. */
+  def run(): Any
+
+  // JMH setup ----------------------------------------------------------------
+
+  @Setup(Level.Trial)
+  def jmhSetup(): Unit = {
+    // Loads the content of ./input/${this.getClass} into this.input
+    import java.nio.charset.StandardCharsets.UTF_8
+    import java.nio.file.{Files, Paths}
+    val jmhName     = this.getClass().getName().toString
+    val className   = jmhName.substring(0, jmhName.indexOf('.')) + jmhName.substring(jmhName.lastIndexOf('.'))
+    val inputFile   = "input/" + className.substring(0, className.length - "_jmhType".length)
+    val inputString = new String(Files.readAllBytes(Paths.get(inputFile)), UTF_8).trim
+    this.input = inputString
+  }
+
+  // Non-JMH setup ------------------------------------------------------------
+
+  /** Runs several interation of the benchmark and dumps output to stdout.
+    * To be called from main methods.
+    */
+  final def batchRun(args: Array[String]): Unit = {
     assert(
       args.length == 4,
       "4 arguments expected: number of batches, batch size, input and expected output")
     val batches   = args(0).toInt
     val batchSize = args(1).toInt
-    val input     = args(2)
+    this.input    = args(2)
     val output    = args(3)
 
-    dump(loop(batches, batchSize, input, output))
+    dump(loop(batches, batchSize, output))
   }
 
   final def dump(times: Array[Long]): Unit = {
@@ -30,7 +53,6 @@ abstract class Benchmark {
 
   final def loop(batches: Int,
                  batchSize: Int,
-                 input: String,
                  output: String): Array[Long] = {
     assert(batches >= 1)
     assert(batchSize >= 1)
@@ -44,7 +66,7 @@ abstract class Benchmark {
 
       var j = 0
       while (j < batchSize) {
-        results(j) = run(input)
+        results(j) = run()
         j += 1
       }
 
---
+++ src/main/scala/jmh-mokes.scala
@@ -0,0 +1,15 @@
+package org.openjdk.jmh.annotations
+
+import scala.Any
+
+class Benchmark
+class State(a: Any)
+class Setup(a: Any)
+
+object Level {
+  def Trial = null
+}
+
+object Scope {
+  def Benchmark = null
+}
